aa5de1cfcc3e90ec51d33523ed53ab512afb4c17
dnguyen0091 <davidnguyen0091@icloud.com>
2024-12-06 19:13:24 -0500

    Initial stage for function to create the header

---
+++ src/header.js
@@ -0,0 +1,54 @@
+
+
+
+export default function Header() 
+{
+    const headerCont= document.querySelector('header');
+
+    // create a logo for the header
+    const logo = document.createElement('img');
+    logo.src = 'https://via.placeholder.com/150';
+    logo.alt = 'Logo';
+    headerCont.appendChild(logo);
+
+    // create a navigation container
+    const navCont = document.createElement('nav');
+    createNav(navCont);
+
+    headerCont.appendChild(navCont);
+
+}
+
+
+//creates tabs for the navigation bar
+function createNav(navCont)
+{
+
+    // create a list that stores navigation links
+    const nav = document.createElement('ul');
+
+    //list item for the home page
+    const home = document.createElement('li');
+    home.textContent = 'Home';
+    nav.appendChild(home);
+
+    //list item for the about page
+    const about = document.createElement('li');
+    about.textContent = 'About';
+    nav.appendChild(about);
+
+
+    //list item for the portfolio page
+    const portfolio = document.createElement('li');
+    portfolio.textContent = 'Portfolio';
+    nav.appendChild(portfolio);
+
+    //list item for the contact page
+    const contact = document.createElement('li');
+    contact.textContent = 'Contact';
+    nav.appendChild(contact);
+
+
+
+    navCont.appendChild(nav);
+}
