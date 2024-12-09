82a1da1e1999747aa84aa210e7ce298565262045
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2024-12-08 19:08:36 -0500

    Handles creation of header and footer so far

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+import Footer from './footer';
+import Header from './Header';
+import './styles.css'; // Import the CSS file
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//         <Header />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+
+
+function main()
+{
+    //call creates the header
+    Header();
+    Footer();
+
+
+    
+}
+
+
+main();
