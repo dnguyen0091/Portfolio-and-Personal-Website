415e23b93d7ce33d17bfee12589ca3d7f58070f5
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2024-12-16 19:10:26 -0500

    Help handle image files better

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,18 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/',
+                        },
+                    },
+                ],
+            },
         ],
     },
     plugins: [
