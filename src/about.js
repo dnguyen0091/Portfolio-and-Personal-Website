7adda42dfd27c66941019a616e9394e73b84042d
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2024-12-18 13:49:31 -0500

    referenced assets

--- src/about.js
+++ src/about.js
@@ -13,27 +13,58 @@ export default function about()
     const profileImg= document.createElement('img');
     profileImg.src = 'https://via.placeholder.com/300';
     profileImg.alt = 'Profile Picture';
-
+    mainCont.appendChild(profileImg);
     // Profile description
     const profileDesc = document.createElement('p');
     profileDesc.id = 'profileDesc';
     profileDesc.textContent= 'I am a Computer Science student at the University of Central Florida where I am learning about software development and computer systems. I am also a self-taught web developer with experience in HTML/CSS, JavaScript, and more. The drive to learn more and master my craft is what motivates me to continue to grow as a developer.When I am not developing a project, I am either working out a the gym or enjoying a game.';
-    
+    mainCont.appendChild(profileDesc);
+
+
     //Append Images of current languages
     const languages = document.createElement('div');
     languages.id = 'languages';
 
     const html = document.createElement('img');
+    html.src='assets/html.svg';
+    html.alt='HTML';
+    languages.appendChild(html);
+
     const css = document.createElement('img');
+    css.src='assets/css.svg';
+    css.alt='CSS';
+    languages.appendChild(css);
+
     const js = document.createElement('img');
+    js.src='assets/js.svg';
+    js.alt='JavaScript';
+    languages.appendChild(js);
+
     const react = document.createElement('img');
+    react.src='assets/react.svg';
+    react.alt='React';
+    languages.appendChild(react);
+
     const java = document.createElement('img');
+    java.src='assets/java.svg';
+    java.alt='Java';
+    languages.appendChild(java);
+
     const python = document.createElement('img');
+    python.src='assets/python.svg';
+    python.alt='Python';
+    languages.appendChild(python);
+
     const c = document.createElement('img');
+    c.src='assets/c.svg';
+    c.alt='C';
+    languages.appendChild(c);
+    
     const pytorch = document.createElement('img');
 
 
-    mainCont.appendChild(profileImg);
-    mainCont.appendChild(profileDesc);
+    
+    
     bodyCont.appendChild(mainCont);
+    bodyCont.appendChild(languages);
 }
